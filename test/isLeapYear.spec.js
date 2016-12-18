b9365c65d02777be1ccc1b523c86587d0afe5980
Gulacsy-Toth <tompascall@gmail.com>
2016-12-18 21:13:03 +0100

    isLeapYear returns true for leap year

--- test/isLeapYear.spec.js
+++ test/isLeapYear.spec.js
@@ -1,12 +1,15 @@
 function isLeapYear (year) {
-	if (year && (year % 100 === 0 || year % 4 !== 0)) {
+	if (year && ( (year % 4 !== 0) || (year % 100 === 0 &&
+		year % 400 !== 0 ))) {
 		return false;
 	}
 	return true;
 }
 
-test('isLeapYear returns true', () => {
+test('isLeapYear returns true for leap year', () => {
 	expect(isLeapYear()).toBe(true);
+	expect(isLeapYear(2004)).toBe(true);
+	expect(isLeapYear(2000)).toBe(true);
 });
 
 test('isLeapYear returns false if input is not leap year', () => {
