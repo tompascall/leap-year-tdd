af3d83b501ae5b4227f69c331db6401198b9eed8
Gulacsy-Toth <tompascall@gmail.com>
2016-12-18 21:26:41 +0100

    isLeapYear throws excepion if it is not called with number

--- test/isLeapYear.spec.js
+++ test/isLeapYear.spec.js
@@ -1,12 +1,18 @@
 function isLeapYear (year) {
-	if (typeof year === 'undefined') {
+
+	if (typeof year !== 'number' || Number.isNaN(year)) {
 		throw Error('isLeapYear must be called with parameter');
 	}
-	
+
 	if (year && ( (year % 4 !== 0) || (year % 100 === 0 &&
 		year % 400 !== 0 ))) {
 		return false;
 	}
+
+	if (typeof year === 'undefined') {
+		throw Error('isLeapYear must be called with parameter');
+	}
+
 	return true;
 }
 
@@ -20,7 +26,12 @@ test('isLeapYear returns false if input is not leap year', () => {
 	expect(isLeapYear(1800)).toBe(false);
 	expect(isLeapYear(1999)).toBe(false);
 });
+
 test('isLeapYear throws excepion if it is called without parameter', () => {
 	expect( () => isLeapYear() ).toThrow();
 });
 
+test('isLeapYear throws excepion if it is not called with number', () => {
+	expect( () => isLeapYear('asdf') ).toThrow();
+	expect( () => isLeapYear(undefined + 0) ).toThrow();
+});
