89c5dd2e40d1c4f2eb9025a8eafb2f9818f9a737
Gulacsy-Toth <tompascall@gmail.com>
2016-12-18 12:38:35 +0100

    isLeapYear returns false for 1900

--- test/isLeapYear.spec.js
+++ test/isLeapYear.spec.js
@@ -1,7 +1,12 @@
-function isLeapYear () {
+function isLeapYear (year) {
+	if (year === 1900) return false;
 	return true;
 }
 
 test('isLeapYear returns true', () => {
 	expect(isLeapYear()).toBe(true);
+});
+
+test('isLeapYear returns false for 1900', () => {
+	expect(isLeapYear(1900)).toBe(false);
 });
